783304527a232b8090b57cc4006ec7eacc611162
blacklotus08 <jettorres08@gmail.com>
2019-05-08 20:58:01 +0800

    2 files modified

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -20,7 +20,7 @@ export class PhotoComponent implements OnInit {
   ngOnInit() {}
 
   searchPhoto() {
-    this.http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2cb5cd49a3d1f65cbd76483c239d3557&text='+ this.photoTitle +'&format=json&nojsoncallback=1&auth_token=72157691311501703-4e2ea8aa1c70c3b0&api_sig=f0c94eaa2ff20af7be9ba0316b6f4be1')
+    this.http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4f4a68e7d4e7e0bf7c00594ecc0964a6&text='+ this.photoTitle +'&format=json&nojsoncallback=1')
     .subscribe((response :Array<any>[]) => {
       
       this.response = response;
