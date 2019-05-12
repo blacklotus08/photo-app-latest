63bbb6e22f8a873f3e54192df2648ebe85ebbda6
blacklotus08 <jettorres08@gmail.com>
2019-05-12 15:12:32 -0700

    6 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Photo App';
+  name = 'Photo Gallery App';
 
   constructor() {}
 
